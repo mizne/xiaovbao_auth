0e0b5513cbed4f4912de05f79cd355518a7d3ba9
mizne <w20054319@126.com>
2018-04-25 19:31:24 +0800

    refactor authorize steps

--- src/routes/oauth.route.ts
+++ src/routes/oauth.route.ts
@@ -6,25 +6,88 @@ import wechatHelper from '../helpers/wechat'
 
 const router = new Router()
 
-router.get('/oauth/authorize', (ctx, next) => {
-  const query = ctx.query
-  if (!ctx.query.redirect_uri) {
-    return ctx.throw('redirect_uri required', 400)
-  }
+export interface OauthQueryOptions {
+  redirect_uri: string
+  response_type: string
+}
 
-  // 开发环境 微信网页授权state有长度限制 故不用token
-  const state = config.environment.production
+let hasSave = false
+
+function fakeCheckOpenID(
+  openid: string
+): Promise<{ hasSave: boolean; user?: Object }> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(hasSave ? { hasSave, user: {} } : { hasSave })
+    }, 100)
+  })
+}
+
+function fakeSaveUserInfo(user: any) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      hasSave = true
+      resolve()
+    }, 2e2)
+  })
+}
+
+function createStateForBaseScope(query: OauthQueryOptions): string {
+  return config.environment.production
     ? jwt.sign(
         {
-          redirect_uri: ctx.query.redirect_uri,
-          response_type: ctx.query.response_type
+          redirect_uri: query.redirect_uri,
+          response_type: query.response_type
         },
         config.jwtSecret,
         {
           expiresIn: '30s'
         }
       )
-    : ctx.query.redirect_uri
+    : query.redirect_uri
+}
+
+function createStateForAutoRedirect(state: string): string {
+  if (config.environment.production) {
+    const decoded: any = jwt.verify(state, config.jwtSecret)
+    return jwt.sign(
+      {
+        redirect_uri: decoded.redirect_uri,
+        response_type: decoded.response_type,
+        auto_redirect: true
+      },
+      config.jwtSecret,
+      { expiresIn: '30s' }
+    )
+  }
+  return state
+}
+
+function extractRedirectUri(state: string): string {
+  if (config.environment.production) {
+    const decoded: any = jwt.verify(state, config.jwtSecret)
+    return decoded.redirect_uri
+  }
+  return state
+}
+
+function hasBeenAutoRedirect(state: string): boolean {
+  if (config.environment.production) {
+    const decoded: any = jwt.verify(state, config.jwtSecret)
+    return decoded.auto_redirect
+  }
+  return false
+}
+
+router.get('/oauth/authorize', (ctx, next) => {
+  const query = ctx.query
+  console.log(`/oauth/authorize`)
+  if (!ctx.query.redirect_uri) {
+    return ctx.throw('redirect_uri required', 400)
+  }
+
+  // 开发环境 微信网页授权state有长度限制 故不用token
+  const state = createStateForBaseScope(ctx.query)
   const url = wechatHelper.getAuthorizeURL(state)
 
   ctx.redirect(url)
@@ -32,9 +95,30 @@ router.get('/oauth/authorize', (ctx, next) => {
 
 router.get('/oauth/wechat-web-oauth', async (ctx, next) => {
   const openid = await wechatHelper.getOpenID(ctx.query.code)
-  const userInfo = await wechatHelper.getUser(openid)
+  const state = ctx.query.state
+  console.log(`/oauth/wechat-web-oauth`)
 
-  // 将用户信息写进数据库
+  // 如果 openid 已被保存 从后台获取 userInfo
+  // 如果未被保存 则 用 snsapi_userinfo 重新授权 获取 userInfo
+
+  const checkUser = await fakeCheckOpenID(openid)
+  if (!checkUser.hasSave) {
+    console.log(`has not save`)
+    if (hasBeenAutoRedirect(state)) {
+      console.log(`has been auto redirect`)
+      const userInfo = await wechatHelper.getUser(openid)
+      const saveUserResult = await fakeSaveUserInfo(userInfo)
+    } else {
+      console.log(`has not been auto redirect`)
+      const redirectUrlForFetchUserInfo = wechatHelper.getAuthorizeURL({
+        state: createStateForAutoRedirect(state),
+        scope: 'snsapi_userinfo'
+      })
+      return ctx.redirect(redirectUrlForFetchUserInfo)
+    }
+  }
+
+  console.log(`finally saved user info`)
 
   const token = jwt.sign(
     {
@@ -45,12 +129,7 @@ router.get('/oauth/wechat-web-oauth', async (ctx, next) => {
       expiresIn: '12h'
     }
   )
-
-  // 开发环境 微信网页授权state有长度限制 故不用token
-  const redirect_uri = config.environment.production
-    ? (jwt.verify(ctx.query.state, config.jwtSecret) as any).redirect_uri
-    : ctx.query.state
-
+  const redirect_uri = extractRedirectUri(state)
   ctx.redirect(
     redirect_uri +
       `?access_token=${token}&openid=${openid}&expires_in=${12 * 60 * 60}`
