2840535b6b122315ffc36f5c5aa0c7fd079be3eb
mizne <w20054319@126.com>
2018-04-25 10:58:29 +0800

    add wechat-oauth

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const Router = require('koa-router')
 const koaBody = require('koa-body')
 const jwt = require('jsonwebtoken')
 const jwtMiddleware = require('koa-jwt')
+const WechatOAuth = require('wechat-oauth')
+const config = require('./config/config')
 
 const app = new Koa()
 const router = new Router()
+const wechatClient = new WechatOAuth(config.wechat.appId, config.wechat.secret)
 
 const jwtSecret = 'xiaovbao_jwt_secret'
 const port = 8000
@@ -34,13 +37,20 @@ app.use(koaBody())
 router.get('/oauth/authorize', (ctx, next) => {
   const query = ctx.query
   console.log(ctx.query)
+  const url = wechatClient.getAuthorizeURL(
+    'redirectUrl',
+    ctx.query.state,
+    'snsapi_base'
+  )
   // const token = jwt.sign(ctx.request.body, jwtSecret, {
   //   expiresIn: '10s'
   // })
-  ctx.body = query
+  ctx.redirect(url)
+  // ctx.body = query
 })
 
 router.get('/wechat_web_oauth', async (ctx, next) => {
+  console.log(`wechat web oauth redirect correct`)
   console.log(ctx.query)
   ctx.body = {
     msg: 'wechat_web_oauth'
