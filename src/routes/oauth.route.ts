a799cf820a1fd09fdc510304c23a70e89302e666
mizne <w20054319@126.com>
2018-04-25 17:57:26 +0800

    add get user

--- src/routes/oauth.route.ts
+++ src/routes/oauth.route.ts
@@ -31,6 +31,7 @@ router.get('/oauth/authorize', (ctx, next) => {
 })
 
 router.get('/oauth/wechat-web-oauth', async (ctx, next) => {
+  console.log(`code: ${ctx.query.code}`)
   const openid = await wechatHelper.getOpenID(ctx.query.code)
   const userInfo = await wechatHelper.getUser(openid)
 
