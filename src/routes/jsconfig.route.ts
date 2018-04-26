a05684064da23a835b64c55a022111a2ac519106
mizne <w20054319@126.com>
2018-04-26 11:56:39 +0800

    add image arraybuffer convert

--- src/routes/jsconfig.route.ts
+++ src/routes/jsconfig.route.ts
@@ -1,5 +1,7 @@
 import * as Router from 'koa-router'
 import wechatHelper from '../helpers/wechat'
+import * as fs from 'fs'
+import axios from 'axios'
 
 const router = new Router()
 
@@ -7,6 +9,37 @@ router.get('/oauth/wechat-jsconfig', async (ctx, next) => {
   const config = await wechatHelper.getJsConfig()
 
   ctx.body = config
+})
+
+router.get('/oauth/image', async (ctx, next) => {
+  const url = ctx.query.url
+  try {
+    const arraybuffer = await axios
+      .request({
+        responseType: 'arraybuffer',
+        url,
+        method: 'get',
+        headers: {
+          'Content-Type': 'image/png'
+        }
+      })
+      .then(result => {
+        console.log(result.data)
+        return result.data
+      })
+
+    ctx.body = {
+      resCode: 0,
+      resMsg: 'fetch arraybuffer ok',
+      result: arraybuffer
+    }
+  } catch (e) {
+    ctx.body = {
+      resCode: -1,
+      resMsg: e.message,
+      resutl: ''
+    }
+  }
 })
 
 export default router
